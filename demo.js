823007d990f42333b7e7a5f6f9cdd1e30b62e9e1
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-08-01 13:57:23 -0700

    remove comments to make js work

--- demo.js
+++ demo.js
@@ -2,34 +2,34 @@
 
 const glyphStates = {
   "♡": "♥",
-  "♥": "♡"
+  "♥": "♡",
 };
 
 const colorStates = {
-  "red" : "",
-  "": "red"
+  red: "",
+  "": "red",
 };
 
 // STEP 1: The line of code below is what lets JavaScript find the elements that
 // we want to make clickable. Without JavaScript, clicking on these heart shapes
-// does nothing. Uncomment the code and refresh the demo page. 
+// does nothing. Uncomment the code and refresh the demo page.
 
-// const articleHearts = document.querySelectorAll(".like-glyph");
+const articleHearts = document.querySelectorAll(".like-glyph");
 
 function likeCallback(e) {
   const heart = e.target;
   mimicServerCall()
-    .then(function(serverMessage){
+    .then(function (serverMessage) {
       // STEP 2: Uncomment the 3 lines after the alert.
       // Here we're using Pillar 1 (DOM Manipulation) to update the screen and
       // mimicking Pillar 3 (Server Communication) to only update the screen if
       // the sending of information to the server succeeds.
       alert("You notified the server!");
-      // alert(serverMessage);
-      // heart.innerText = glyphStates[heart.innerText];
-      // heart.style.color = colorStates[heart.style.color];
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
     })
-    .catch(function(error) {
+    .catch(function (error) {
       alert("Something went wrong!");
     });
 }
@@ -38,11 +38,11 @@ function likeCallback(e) {
 // work, we need to add a click event listener to the elements we identified in
 // STEP 1. That's Pillar 2, event handling. Uncomment this code:
 
-// for (const glyph of articleHearts) {
-//   glyph.addEventListener("click", likeCallback);
-// }
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
-// STEP 4: 
+// STEP 4:
 
 // When all the STEPs' code changes have been complete, you'll be able to see a
 // working demonstration of our reference example. Sure, it's maybe not as
@@ -54,8 +54,8 @@ function likeCallback(e) {
 //------------------------------------------------------------------------------
 
 function mimicServerCall() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       resolve("Pretend remote server notified of action!");
     }, 300);
   });
